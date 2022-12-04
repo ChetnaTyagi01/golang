511c9c1b52f4566302dc9edfa06f796db8346d7d
Chetna.Tyagi01@outlook.com <Chetna.Tyagi01@outlook.com>
2022-12-04 19:18:28 +0530

    Using pointers

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// using pointers as arguments to access the data within the underying functions
+func updateName(x *string) {
+	*x = "wedge"
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	name := "tifa"
+
+	fmt.Println("memory address of name:", &name) // &name gets a pointer to the memory location of name variable
+	m := &name                                    // m itself has its own memory address but is currently storing the memory address of name variable
+	fmt.Println("memory address:", m)
+	fmt.Println("value at memory address:", *m) // using * infront of a pointer gets the value that it points to
+
+	fmt.Println("before:", name) // tifa
+	updateName(m)
+	fmt.Println("after:", name) // wedge
 }
