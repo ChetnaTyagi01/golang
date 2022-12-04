c6d8c8d93286f08c96d236b19f50e872d82a09c6
Chetna.Tyagi01@outlook.com <Chetna.Tyagi01@outlook.com>
2022-12-04 19:58:49 +0530

    Understanding Pass by value

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # golang 
+
+# for running multiple files in go specify all the file names you need to run
+# in our case under directory /package_scope , run the below command
+# go run main.go greetings.go
+
 # go is a pass-by-value language
 # go makes copies of values when passed into functions - when we pass variables as arguments in function go makes a copy of those values for the function to use
 # In go, variables can be split into two groups - 
 # Group A (Non-Pointer values) - Strings, Ints, Floats, Booleans, Arrays, Structs
 # Group B (Pointer wrapper values)- Slices, Maps, Functions
-# 
--- main.go
+++ main.go
@@ -2,29 +2,6 @@ package main
 
 import "fmt"
 
-// in the below function x is the copy of the name variable and not the original name variable
-func updateName(x string) {
-	x = "ria" // so here we are just updating the copy and not the original value
-	// return x // if we want to change the original value we can return the value from here
-}
-
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99 // updates the original map
-}
 func main() {
-	// group A types -  strings, ints, floats, boolean, arrays, structs
-	name := "john"
-	updateName(name)
-	fmt.Println(name) // output - john
-	// since go is a pass by value language
-	// everytime we pass a value/variable into a function, go creates a copy of the variable
-
-	// group B types - slices, maps, functions
-	menu := map[string]float64{
-		"pie":     106.09,
-		"pudding": 215.02,
-	}
-	updateMenu(menu)
-	fmt.Println(menu)
-	// for group B types, go creates a copy of the pointer which points to the same value hence, the original value gets changed
+	fmt.Println("Hello world!")
 }
