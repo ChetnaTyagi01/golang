7e1853c5aeae7fc5a31163c890425844f5ae76d0
Chetna.Tyagi01@outlook.com <Chetna.Tyagi01@outlook.com>
2022-12-04 19:53:31 +0530

    Package Scopes

--- main.go
+++ main.go
@@ -2,14 +2,6 @@ package main
 
 import "fmt"
 
-var scoreOne = 99.2
-
-// main() function only appears once in the program and is an entry point to the application
 func main() {
-	// var scoreTwo = 97.5 // can't be accessed in any other file
-	sayHello("chetna") // using other file's function and variables here
-	for _, v := range points {
-		fmt.Println(v)
-	}
-	showScore()
+	fmt.Println("Hello world!")
 }
